c2cd6acf52eebc966de4478e2d0b7749faea46b5
Stas Tolstonozhenko <stastolsto@gmail.com>
2023-11-27 22:28:34 +0200

    Delete types from preload

--- electron/src/preload.ts
+++ electron/src/preload.ts
@@ -1,3 +1,4 @@
+import { FunctionMap, Types, TypesKeys } from 'Types/index';
 import { contextBridge, ipcRenderer } from 'electron';
 
 const test = getRender();
@@ -12,26 +13,8 @@ function getRender(): FunctionMap {
     ]
     arrayHandels.forEach(element => {
         obj[element] = (arrgument: Types[typeof element]) => 
-        ipcRenderer.invoke(element, arrgument) as Promise<TypesReturn[typeof element]>;
+        ipcRenderer.invoke(element, arrgument) ;
         
     });
     return obj;
 }
-
-type Types = {
-    getSomeData: boolean,
-    getSomeData1: boolean
-}
-
-type TypesReturn = {
-    getSomeData: number,
-    getSomeData1: boolean
-}
-
-type FunctionMap = {
-    [key: string]: (arr: Types[TypesKeys]) => void;
-};
-
-type TypesKeys = keyof Types;
-
-
